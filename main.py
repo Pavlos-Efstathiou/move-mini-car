49eb27fed5c2e51cce0d2c456a3e3d7a6ee33ca9
Pavlos Efstathiou <Jimosmekma@gmail.com>
2020-12-18 21:51:39 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showString("F")
-    kitronik_servo_lite.setDistancePerSecond(200)
+def on_button_pressed_a():
+    basic.show_string("F")
+    kitronik_servo_lite.set_distance_per_second(200)
     kitronik_servo_lite.forward()
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showString("B")
-    kitronik_servo_lite.setDistancePerSecond(200)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_gesture_free_fall():
+    pass
+input.on_gesture(Gesture.FREE_FALL, on_gesture_free_fall)
+
+def on_button_pressed_ab():
+    global Brightness_Value
+    if Brightness_Value <= 255:
+        Brightness_Value -= 50
+    if Brightness_Value <= 120:
+        Brightness_Value += 50
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    basic.show_string("B")
+    kitronik_servo_lite.set_distance_per_second(200)
     kitronik_servo_lite.backward()
-})
-let Pixel_Array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB_RGB)
-Pixel_Array.setBrightness(1000)
-led.setBrightness(255)
-basic.forever(function () {
-    Pixel_Array.showRainbow(0, 255)
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        `)
-})
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+Brightness_Value = 0
+Brightness_Value = led.brightness()
+Pixel_Array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB_RGB)
+Pixel_Array.set_brightness(1000)
+Pixel_Array.show_rainbow(0, 255)
+led.set_brightness(255)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,17 +1,52 @@
+function right (degrees: number) {
+    kitronik_servo_lite.turnRight(degrees)
+}
+function left (degrees: number) {
+    kitronik_servo_lite.turnLeft(degrees)
+}
 input.onButtonPressed(Button.A, function () {
     basic.showString("F")
     kitronik_servo_lite.setDistancePerSecond(200)
-    kitronik_servo_lite.forward()
+    left(90)
+    forwards(0)
 })
+input.onGesture(Gesture.FreeFall, function () {
+    basic.showString("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+})
+function backwords (distance: number) {
+    if (distance == 0) {
+        kitronik_servo_lite.backward()
+    } else {
+        kitronik_servo_lite.driveBackwards(distance)
+    }
+}
 input.onButtonPressed(Button.B, function () {
     basic.showString("B")
     kitronik_servo_lite.setDistancePerSecond(200)
-    kitronik_servo_lite.backward()
+    backwords(0)
 })
+function forwards (distance: number) {
+    if (distance == 0) {
+        kitronik_servo_lite.forward()
+    } else {
+        kitronik_servo_lite.driveForwards(distance)
+    }
+}
+let Brightness_Value = led.brightness()
 let Pixel_Array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB_RGB)
 Pixel_Array.setBrightness(1000)
 Pixel_Array.showRainbow(0, 255)
 led.setBrightness(255)
+forwards(50)
+basic.pause(1000)
+basic.showIcon(IconNames.Heart)
+basic.pause(1000)
 basic.forever(function () {
-	
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
 })
