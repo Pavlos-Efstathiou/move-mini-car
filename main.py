81923b92f71572245155b94869429330afd17e6e
Pavlos Efstathiou <Jimosmekma@gmail.com>
2020-12-18 20:25:41 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 def on_button_pressed_a():
+    basic.show_string("F")
+    kitronik_servo_lite.set_distance_per_second(200)
     kitronik_servo_lite.forward()
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    basic.show_string("B")
+    kitronik_servo_lite.set_distance_per_second(200)
+    kitronik_servo_lite.backward()
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
 Pixel_Array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB_RGB)
 
 def on_forever():
-    Pixel_Array.show_rainbow(1, 360)
+    Pixel_Array.set_brightness(1000)
+    Pixel_Array.show_rainbow(0, 255)
     basic.pause(2000)
-    Pixel_Array.shift(1)
+    basic.show_string("" + str((Pixel_Array.power())))
+    basic.pause(2000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,10 +1,23 @@
 input.onButtonPressed(Button.A, function () {
+    basic.showString("F")
     kitronik_servo_lite.setDistancePerSecond(200)
     kitronik_servo_lite.forward()
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+    kitronik_servo_lite.setDistancePerSecond(200)
+    kitronik_servo_lite.backward()
+})
 let Pixel_Array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB_RGB)
+Pixel_Array.setBrightness(1000)
+led.setBrightness(255)
 basic.forever(function () {
-    Pixel_Array.showRainbow(1, 255)
-    basic.pause(2000)
-    Pixel_Array.shift(4)
+    Pixel_Array.showRainbow(0, 255)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
 })
