348c2a23db5edb9c1b240dd11fb7c64b5d4c0298
Pavlos Efstathiou <Jimosmekma@gmail.com>
2020-12-18 17:49:48 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    pins.servoWritePin(AnalogPin.P1, 180)
-    pins.servoWritePin(AnalogPin.P2, 0)
+    pins.servoWritePin(AnalogPin.P1, 90)
+    pins.servoWritePin(AnalogPin.P2, 90)
 })
 let Pixel_Array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB_RGB)
 basic.forever(function () {
     Pixel_Array.showRainbow(1, 360)
+    basic.pause(2000)
 })
